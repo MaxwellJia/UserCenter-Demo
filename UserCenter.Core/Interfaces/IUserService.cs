afd6861110ea858336d6cae27330a662a179e33d
BillFBWong <hyf050416@gmail.com>
2025-04-25 19:52:51 +1000

    Update project structure, add Infrastructure layer.

---
+++ UserCenter.Core/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserCenter.Core.Models;
+
+namespace UserCenter.Core.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerator<User>> GetAllUsersAsync();
+        Task<User?> GetUserByIdAsync(int id);
+        Task<User> CreateUserAsync(User user);
+        Task<User> UpdateUserAsync(User user);
+        Task<bool> DeleteUserAsync(int id);
+    }
+}
