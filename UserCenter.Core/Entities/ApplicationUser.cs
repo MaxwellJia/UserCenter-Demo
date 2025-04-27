7a4ea6c99f0d4329cbd6543fe6c68eff3e8ff40e
MaxwellJia <591790421@qq.com>
2025-04-27 15:12:03 +1000

    Update ApplicationUser to use Microsoft.AspNetCore.Identity;

--- UserCenter.Core/Entities/ApplicationUser.cs
+++ UserCenter.Core/Entities/ApplicationUser.cs
@@ -3,25 +3,16 @@ using System.Collections.Generic;
 using System.Security.Principal;
 using Microsoft.AspNetCore.Identity;
 
+
 namespace UserCenter.Core.Entities;
 
 public class ApplicationUser : IdentityUser<Guid>
 {
-    public long Id { get; set; }
-
-    public string Username { get; set; }
-
-    public string UserAccount { get; set; }
+    public string NickName { get; set; }
 
     public string AvatarUrl { get; set; }
 
     public short? Gender { get; set; }
-
-    public string UserPassword { get; set; }
-
-    public string Phone { get; set; }
-
-    public string Email { get; set; }
 
     public int? UserStatus { get; set; }
 
