500490c7fc8c65722543d3fdc396d77b6c0f6920
MaxwellJia <591790421@qq.com>
2025-04-27 14:54:58 +1000

    Update Microsoft.AspNetCore.Identity for user entity

---
+++ UserCenter.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Identity;
+
+namespace UserCenter.Core.Entities;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public long Id { get; set; }
+
+    public string Username { get; set; }
+
+    public string UserAccount { get; set; }
+
+    public string AvatarUrl { get; set; }
+
+    public short? Gender { get; set; }
+
+    public string UserPassword { get; set; }
+
+    public string Phone { get; set; }
+
+    public string Email { get; set; }
+
+    public int? UserStatus { get; set; }
+
+    public DateTime? CreateTime { get; set; }
+
+    public DateTime? UpdateTime { get; set; }
+
+    public short? IsDelete { get; set; }
+
+    public int? UserRole { get; set; }
+}
