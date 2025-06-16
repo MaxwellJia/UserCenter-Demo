9206304b8e31c09514ce55d12cb8b3baa0da8c11
MaxwellJia <591790421@qq.com>
2025-06-16 22:10:03 +1000

    Delete static frontend

--- UserCenter.API/Program.cs
+++ UserCenter.API/Program.cs
@@ -171,10 +171,6 @@ namespace UserCenter.API
                     }
                 }
             }
-            //确保 .NET 后端项目支持静态文件访问 将前端也放在web app service上
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseRouting();
 
             // Allow all frontend access
             app.UseCors("AllowFrontend");
@@ -184,7 +180,6 @@ namespace UserCenter.API
             app.UseAuthorization();
 
             app.MapControllers();
-            app.MapFallbackToFile("index.html");
 
             app.Run();
         }
